fc9dd98e0ac68668b3583fd91f158d258150a81a
khyiin <revechequintenita@gmail.com>
2025-10-16 09:30:53 +0800

    web

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
-# Temporary in-memory "database"
+
 users = []
 
 @app.route('/')
